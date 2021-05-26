0a0ab4d5fb536e3e5de503fcc570f0571425e4d9
Marek <154400@student.uwm.edu.pl>
2021-05-27 00:09:58 +0200

    wygląd zmiany hasła

--- Przychodnia/resources/views/auth/changePassword.blade.php
+++ Przychodnia/resources/views/auth/changePassword.blade.php
@@ -4,97 +4,88 @@
 
 <div class="container">
 
-    <div class="row justify-content-center">
+    <div class="login">
 
-        <div class="col-md-8">
+        <div class="login-content">
+        
+            <div class = "change-board">
 
-            <div class="card">
-                <div style="text-align:center">
-                <div class="card-header">Zmień hasło</div>
-            </div>
-   
+                <h2>Zmień hasło</h2>
 
-                <div class="card-body">
+                        <form method="POST" action="{{ route('change.password') }}">
 
-                    <form method="POST" action="{{ route('change.password') }}">
+                            <div > 
 
-                        @csrf 
+                                @csrf 
 
-   
+        
 
-                         @foreach ($errors->all() as $error)
+                                @foreach ($errors->all() as $error)
+            
+                                    <p class="text-danger">{{ $error }}</p>
 
-                            <p class="text-danger">{{ $error }}</p>
-
-                         @endforeach 
-
-  
-
-                        <div class="form-group row">
-
-                            <label for="password" class="col-md-4 col-form-label text-md-right">Current Password</label>
-
-  
-
-                            <div class="col-md-6">
-
-                                <input id="password" type="password" class="form-control" name="current_password" autocomplete="current-password">
+                                @endforeach 
 
                             </div>
 
-                        </div>
+                            <div class="txt-field">
 
-  
+                                <label for="password">Aktualne hasło</label>
 
-                        <div class="form-group row">
+                                <div class="col-md-14">
 
-                            <label for="password" class="col-md-4 col-form-label text-md-right">New Password</label>
+                                    <input id="password" type="password" class="form-control" name="current_password" autocomplete="current-password">
 
-  
-
-                            <div class="col-md-6">
-
-                                <input id="new_password" type="password" class="form-control" name="new_password" autocomplete="current-password">
+                                </div>
 
                             </div>
-
-                        </div>
-
-  
-
-                        <div class="form-group row">
-
-                            <label for="password" class="col-md-4 col-form-label text-md-right">New Confirm Password</label>
 
     
 
-                            <div class="col-md-6">
+                            <div class="txt-field">
 
-                                <input id="new_confirm_password" type="password" class="form-control" name="new_confirm_password" autocomplete="current-password">
+                                <label for="password" >Nowe hasło</label>
 
-                            </div>
+    
 
-                        </div>
+                                <div class="col-md-14">
 
-   
+                                    <input id="new_password" type="password" class="form-control" name="new_password" autocomplete="current-password">
 
-                        <div class="form-group row mb-0">
-
-                            <div class="col-md-8 offset-md-4">
-
-                                <button type="submit" class="btn btn-primary">
-
-                                    Update Password
-
-                                </button>
+                                </div>
 
                             </div>
 
-                        </div>
+    
 
-                    </form>
+                            <div class="txt-field">
 
-                </div>
+                                <label for="password"> Potwierdź nowe hasło</label>
+
+        
+
+                                <div class="col-md-14">
+
+                                    <input id="new_confirm_password" type="password" class="form-control" name="new_confirm_password" autocomplete="current-password">
+
+                                </div>
+
+                            </div>
+
+    
+
+
+                             <div class="button-section">
+
+                                <div class="right">
+
+                                    <button type="submit">{{ __('Zmień hasło') }}</button>
+
+                                </div>
+
+                            </div>
+
+                        </form>
 
             </div>
 
