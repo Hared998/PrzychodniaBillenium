be645681d4ef33b77981705fe3fcb03b9d0c1f7b
mimichal1998 <sholender@billennium.pl>
2021-06-09 20:10:14 +0200

    Dodanie widoku do tworzenia recept

---
+++ Przychodnia/resources/views/tworzenieRecepty.blade.php
@@ -0,0 +1,31 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="recepta-content">
+    <div class="row">
+        <div class="recepta-lekarz col-3 text-center">
+            <h3>Utwórz</h3>
+            <h3>receptę</h3>
+        </div>
+        <div class="recepta-content col-9 d-flex justify-content-center">
+            <div class="recepta-board d-flex justify-content-center">
+                <form action="#" id="tworzenie-wizyt__form">
+                    <div>
+                        <label for="przepisane-leki">Przepisane leki</label>
+                        <input type="text" id="przepisane-leki" name="przepisane-leki">
+                    </div>
+                    <div>
+                        <label for="zalecenia">Zalecenia</label>
+                        <input type="text" id="zalecenia" name="zalecenia">
+                    </div>
+                    <input type="text" id="imie-nazwisko-pacjenta" value="" class="d-none">
+                    <input type="text" id="data-wystawienia-recepty" value="" class="d-none">
+                    <input type="text" id="wystawiajacy-lekarz" value="" class="d-none">
+
+                    <button type="submit" class="tworzenie-wizyt__button">Utwórz</button>
+                </form>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
