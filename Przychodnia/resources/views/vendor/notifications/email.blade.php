129968a10698439b8f6b1481a6733d334a3b0eae
Marek <154400@student.uwm.edu.pl>
2021-06-10 00:23:17 +0200

    tłumaczenie maila

---
+++ Przychodnia/resources/views/vendor/notifications/email.blade.php
@@ -0,0 +1,60 @@
+@component('mail::message')
+{{-- Greeting --}}
+@if (! empty($greeting))
+# {{ $greeting }}
+@else
+@if ($level === 'error')
+# @lang('Whoops!')
+@else
+# @lang('No sieema mordeczko!')
+@endif
+@endif
+
+{{-- Intro Lines --}}
+@foreach ($introLines as $line)
+{{ 'ojojojoj aaa... aaa kogo my tu mamy?' }}
+
+@endforeach
+
+{{-- Action Button --}}
+@isset($actionText)
+<?php
+    switch ($level) {
+        case 'success':
+        case 'error':
+            $color = $level;
+            break;
+        default:
+            $color = 'primary';
+    }
+?>
+@component('mail::button', ['url' => $actionUrl, 'color' => $color])
+{{'Kliknij tutej... bedzie działać'}}
+@endcomponent
+@endisset
+
+{{-- Outro Lines --}}
+
+{{ 'Miłego korzystania z serwisu' }}
+
+
+
+{{-- Salutation --}}
+@if (! empty($salutation))
+{{ $salutation }}
+@else
+@lang('Pozdróweczki, SzpachlaTeam')<br>
+@endif
+
+{{-- Subcopy --}}
+@isset($actionText)
+@slot('subcopy')
+@lang(
+    "Jeśli masz problem z nacisnięciem przycisku \":actionText\" skopiuj adres URL następnie wklej go w swojej przeglądarce:\n",
+    [
+        'actionText' => $actionText,
+    ]
+) <span class="break-all">[{{ $displayableActionUrl }}]({{ $actionUrl }})</span>
+@endslot
+@endisset
+@endcomponent
